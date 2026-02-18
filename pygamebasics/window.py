408a02f6624613f7698ac1da7fc11ad585c2474e
MottenNutki-1401 <202310763@gordoncollege.edu.ph>
2026-02-19 01:38:13 +0800

    create basic window in pygame

--- pygamebasics/window.py
+++ pygamebasics/window.py
@@ -16,3 +16,19 @@ pygame.display.set_caption("For You....")
 # When you run this code a window will pop up but will close automatically
 # this needs a game loop that runs continously and updates constantly 
 #tuple=> an ordered collection of objects
+
+#pygame game loop =>update frames so it wont close in execution
+runnings = True #runnings here is a variable (not reserved)
+while runnings: #this will be the heartbeat of the code => !this == nothing to execute
+    for eventi in pygame.event.get(): #eventi is just a variable
+    #"repeat|something|each thing we receive fron pygame(eventi) in => from this collection"
+    #pygame=>library .event=>eventsystem .get()=>give all events that happened e.g mouse click
+        if eventi.type == pygame.QUIT:
+           runnings = False
+    
+    win.fill((155,100,200))
+    
+    
+    pygame.display.update()
+pygame.quit() #Very IMPORTANT => indention must be outside of loop structure
+
